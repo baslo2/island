89d0274022a347a95f246ea57956a0bbcb9d708c
Elmir Shamsutdinov <shamsut.elm@gmail.com>
2025-08-26 19:27:08 +0300

    added init animals logic

--- src/main/java/island/Island.java
+++ src/main/java/island/Island.java
@@ -1,13 +1,27 @@
 package island;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 import island.model.animals.Animal;
+import island.model.animals.AnimalFactory;
+import island.model.animals.AnimalType;
 
 public class Island {
 
-    private final IslandNode[][] map = new IslandNode[100][20];
+    public Island() {
+        this(100, 20);
+    }
+
+    public Island(int x, int y) {
+        map = new IslandNode[x][y];
+    }
+
+    private static final Random r = new Random();
+
+    private final IslandNode[][] map;
 
     public void init() {
         int x = map.length;
@@ -29,6 +43,32 @@ public class Island {
 
     public int getYLength() {
         return map[0].length - 1;
+    }
+
+    public void initAnimals(int animalsValue) {
+        LinkedList<Animal> animals = new LinkedList<>();
+        for (AnimalType type : AnimalType.values()) {
+            animals.addAll(AnimalFactory.getNewAnimals(type, animalsValue));
+        }
+
+        while (!animals.isEmpty()) {
+            var animal = animals.pop();
+            // TODO refactor me
+            while (!addAnimalInRandomNode(animal)) {
+            }
+        }
+    }
+
+    private boolean addAnimalInRandomNode(Animal animal) {
+        return getIslandNode(getRandomXLocation(), getRandomYLocation()).addAnimal(animal);
+    }
+
+    private int getRandomXLocation() {
+        return r.nextInt(0, map.length);
+    }
+
+    private int getRandomYLocation() {
+        return r.nextInt(0, map[0].length);
     }
 
     public List<Animal> getAllAnimals() {
